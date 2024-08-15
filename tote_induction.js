bcf7c9c50599b5ef2f6fa1d1229a2a4eba44da90
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-15 08:23:11 -0500

    tote_induction.js

---
+++ tote_induction.js
@@ -0,0 +1,22 @@
+/*
+This script applies to a specific screen
+in the application that asks for a barcode,
+scans the barcode, and then sends a tab key
+to move to the next field. There is a sliught
+delay between the scan and the auto-entered tab
+key to allow the application to process the scan
+*/
+
+function onScan(scanData) {
+    // Process the scan data here
+    Device.beep(50, 50, 50);
+    // After processing the scan data, wait for 150 milliseconds 
+    // before sending the tab character
+    setTimeout(function() {
+        //View.toast("Sending Tab Key");
+        Device.sendKeys('{HEX:0009}');
+        //View.toast("Tab Key Sent");
+    }, 150);
+}
+
+WLEvent.on("Scan", onScan);
