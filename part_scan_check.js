19f411fe1b8a3e2c9e835192736e1fe66b3736a7
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-15 11:02:26 -0500

    initial commit

---
+++ part_scan_check.js
@@ -0,0 +1,10 @@
+/*
+This script checks the part number scanned by the user.
+The part number should either be the UPC (12 digits long) 
+or the EAN (13 digits long) of the product. If the scanned
+part number is neither of these, the script displays an
+error message and prompts the user to rescan the barcode.
+This is simillar to the serial_check.js script, but with
+different length and format requirements.
+*/
+
