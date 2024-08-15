7beb2a3c4df1c65e7c383b4e7e81411cd70ba6cb
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-15 12:44:33 -0500

    general code structure/logic

--- workzone.js
+++ workzone.js
@@ -6,5 +6,21 @@ only enters the wildcard character, the script will display an error
 message and prompt the user to enter more characters.
 */
 
-View.toast("Test");
+View.toast("Script Working.");
 
+var zoneOne = Screen.getText(4, 6, 4);
+
+function onScan(event) {
+    // Check if something is inserted on 
+    // the Workzone field
+    if (zoneOne === "*") {
+        View.toast("Step 2 working.");
+        // If the user only enters the wildcard character, display an error message
+        Device.beep(2000, 1000, 50);
+        View.toast("Please enter more characters.");
+        // Clear the scanned data
+        event.data = "";
+    }
+}
+
+WheelEvent.on("Scan", onScan);
