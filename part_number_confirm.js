a210d29d3f07215738b84c0563b2924f60e0119a
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-15 09:42:53 -0500

    Added part_number_confirm.js

---
+++ part_number_confirm.js
@@ -0,0 +1,21 @@
+/*
+This script applies to a screen that asks the user to
+confirm the part number of a product. The part number
+is a string of numbers and letters that is generally
+8 characters long.
+
+The part number the user is looking for is displayed
+at the top of the screen. The user is supposed to scan
+the barcode containing the part number and confirm that
+it matches the displayed part number.
+
+The script checks if the scanned part number matches the
+displayed part number. If the part number matches, the
+script allows the user to proceed to the next screen.
+If the part number does not match, the script displays
+an error message and prompts the user to rescan the
+barcode.
+
+8/15/2024
+*/
+
