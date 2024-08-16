590486d59b7aa0ad80b90a68c8282b9bd86b088c
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-16 07:28:51 -0500

    Added a slight delay in scan process/check the length of the workzone field

--- workzone.js
+++ workzone.js
@@ -8,22 +8,29 @@ message and prompt the user to enter more characters.
 
 View.toast("Script Working.");
 
-var zoneOne = Screen.getText(4, 6);
-if (zoneOne === "*") {
-    View.toast("Please enter more characters.");
-}
-
 function onScan(event) {
-    // Check if something is inserted on 
-    // the Workzone field
-    if (zoneOne === "") {
-        View.toast("Step 2 working.");
-        // If the user only enters the wildcard character, display an error message
-        Device.beep(2000, 1000, 50);
+    var zoneOne = Screen.getText(4, 6);
+    // Check if the input is only a wildcard character
+    if (zoneOne === "*") {
         View.toast("Please enter more characters.");
         // Clear the scanned data
         event.data = "";
+        return;
     }
+
+    // TODO: Add a delay after the scan event
+    // to give the scan time to process the data
+
+    setTimeout(function() {
+        if (zoneOne.length <= 1) {
+            View.toast("Step 2 working.");
+            // If the user only enters the wildcard character, display an error message
+            Device.beep(2000, 1000, 50);
+            View.toast("Please enter more characters.");
+            // Clear the scanned data
+            event.data = "";
+        }
+    }, 1000); // Adjust the delay as needed
 }
 
-WheelEvent.on("Scan", onScan);
+Screen.on("Scan", onScan);
