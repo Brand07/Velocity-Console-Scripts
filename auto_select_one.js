b74ea7585ed90490f985cbe639df4cccf953d50d
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-26 13:24:07 -0500

    Comment toast message - confirmed working

--- auto_select_one.js
+++ auto_select_one.js
@@ -2,5 +2,5 @@
 Sends a "1" when reaching the "Welcome to WM Provia Screen",
 which automatically selects the first option in the list.
  */
-View.toast("Auto selecting first option");
+//View.toast("Auto selecting first option");
 Device.sendKeys("{1}");
