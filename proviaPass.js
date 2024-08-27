b5fd4ce1d62615543f66d97fabec9a44e5397c17
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-27 06:53:16 -0500

    Function change

--- proviaPass.js
+++ proviaPass.js
@@ -1,9 +1,4 @@
-function onScreenLoad(event) {
- setTimeout(function() {
-  View.toast("Sending '1' key.")
-  Device.sendKeys("1");
- }, 150); // 150ms delay
-}
-
-// Register the onScreenLoad function to handle the screen load event
-WLEvent.on("ScreenLoad", onScreenLoad);
+View.toast("Auto selecting first option");
+setTimeout(function() {
+    Device.sendKeys("1");
+}, 150); // 150ms delay
