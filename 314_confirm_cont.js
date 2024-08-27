97034c5e94cc4a0d4dfa72f7af4d61ac75b65b33
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-27 08:33:37 -0500

    Change variables for clarification

--- 314_confirm_cont.js
+++ 314_confirm_cont.js
@@ -1,16 +1,17 @@
 View.toast("Please scan the container number.");
-function onScan(scanData) {
+function onScan(event) {
     // Process the scan data
-    if (scanData.startsWith("0000")) {
+    const data = event.data;
+    if (data.startsWith("0000")) {
         View.toast("Container number scanned.");
         Device.beep(25, 25, 25);
         setTimeout(function() {
             Device.sendKeys("{HEX:0009}");
         }, 100);
     } else {
+        event.data = "";
         Device.beep(2000, 1000, 50);
         View.toast("Invalid scan. Please rescan.");
-        scanData = "";
     }
 }
 
