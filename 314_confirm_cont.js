5ba36e83854e6f37ffd2c170ef675c6c2df06adc
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-27 08:22:20 -0500

    Add { on else statement

--- 314_confirm_cont.js
+++ 314_confirm_cont.js
@@ -1,15 +1,15 @@
 function onScan(scanData) {
-    //Process the scan data
+    // Process the scan data
     if (scanData.startsWith("0000")) {
         Device.beep(25, 25, 25);
         setTimeout(function() {
             Device.sendKeys("{HEX:0009}");
         }, 100);
-    }
-    else {
+    } else {
         Device.beep(2000, 1000, 50);
         View.toast("Invalid scan. Please rescan.");
         scanData = "";
     }
 }
+
 WLEvent.on("Scan", onScan);
