5173ac429580aea2dbc071878e913e1d07623744
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-30 08:00:19 -0500

    Add description comments

---
+++ Screen_315/315_tote_id_check.js
@@ -0,0 +1,8 @@
+/*
+Date: August 30th, 2024
+Author: Brandon Yates
+The purpose of this script is to not only check
+for the 'tot' prefix in the scanned barcode, but
+also auto-tab to the workzone field and auto-enter
+'*101 or %101' into the workzone field.
+ */
