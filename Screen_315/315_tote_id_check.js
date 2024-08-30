c87bcbaa6385e57cdaa62e51fc0759141a8f9f7a
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-30 08:04:21 -0500

    Add logic as described

--- Screen_315/315_tote_id_check.js
+++ Screen_315/315_tote_id_check.js
@@ -6,3 +6,30 @@ for the 'tot' prefix in the scanned barcode, but
 also auto-tab to the workzone field and auto-enter
 '*101 or %101' into the workzone field.
  */
+
+//View.toast("2nd Script started");
+
+function onScan(event) {
+    // See if the input matches "TOT"
+    if (event.data.startsWith("TOT")) {
+        setTimeout(function() {
+            //View.toast("Sending Enter Key");
+            Device.sendKeys('{HEX:0009}');
+            //View.toast("Enter Key Sent");
+        }, 100);
+        setTimeout(function() {
+            //View.toast("Sending Workzone");
+            Device.sendKeys('*101');
+            //View.toast("Setting Workzone");
+        }, 150);
+    }
+    else {
+        View.toast("Not a Tote ID");
+        event.data = "";
+    }
+}
+
+WLEvent.on("Scan", onScan);
+
+
+
