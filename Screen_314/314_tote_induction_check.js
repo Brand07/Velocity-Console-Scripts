2a2c4ebbf767297ff9da9aacda1c6c81a8f2150e
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-11 14:54:57 -0500

    Add alternative solution

--- Screen_314/314_tote_induction_check.js
+++ Screen_314/314_tote_induction_check.js
@@ -16,5 +16,18 @@ function onScan(event) {
     }
 }
 
+function onEnterKey(event) {
+    var tote_field = Screen.getText(6, 0, 8);
+
+    // Check if the tote field starts with "0" or does not start with "TOT"
+    if (tote_field.startsWith("0") || !tote_field.startsWith("TOT")) {
+        event.data = "";
+        View.toast("Please scan a valid tote ID.");
+        Device.beep(250, 75, 50);
+        event.preventDefault();
+        event.stopPropagation();
+    }
+}
+
 WLEvent.on("Scan", onScan);
-WLEvent.on("OnKey<000D>", onScan);
+WLEvent.on("OnKey<000D>", onEnterKey);
