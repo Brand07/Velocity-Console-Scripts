40711ba78e61400d81f7a2c94b544673463ce085
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-11 14:05:35 -0500

    Add delay

--- Screen_314/314_tote_induction_check.js
+++ Screen_314/314_tote_induction_check.js
@@ -22,11 +22,12 @@ function toteCheck(event) {
     // Check that the tote field does not start with "TOT"
     var tote_field = Screen.getText(6, 0, 8);
     if (!(tote_field.substring(0, 3) === 'TOT')) {
-        // Add your logic here
-        View.toast("Please scan a valid tote ID.");
-        Device.beep(2000, 1000, 50);
-        event.data = "";
-
+        setTimeout(function() {
+            // Add your logic here
+            View.toast("Please scan a valid tote ID.");
+            Device.beep(2000, 1000, 50);
+            event.data = "";
+        }, 1000); // 1-second delay
     }
 }
 
