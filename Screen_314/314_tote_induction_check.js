77636f41c87f8d154e442b74f4d150973c2f595d
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-12 09:22:31 -0500

    Add prompt option

--- Screen_314/314_tote_induction_check.js
+++ Screen_314/314_tote_induction_check.js
@@ -18,18 +18,21 @@ function onScan(event) {
     }
 }
 
-function onEnterKey(event) {
+function toteCheck(event) {
     var tote_field = Screen.getText(6, 0, 8);
-
-    // Check if the tote field starts with "0" or does not start with "TOT"
     if (tote_field.startsWith("0") || !tote_field.startsWith("TOT")) {
-        Prompt.promptOptions("Invalid tote ID. Please scan a valid tote ID.", ["OK"], function() {
-            event.data = "";
-            Device.beep(250, 75, 50);
-            WLEvent.off("OnKey<000D>", onEnterKey);
-        });
+        View.toast("Please scan a valid tote ID.");
+        Device.beep(250, 75, 50);
+        Prompt.promptOptions("Tote ID Is Invalid", "Please scan a valid Tote ID", "OK|CLOSE", function (result) {
+            if (result === "OK") {
+                event.data = "";
+            } else {
+                Device.sendkeys("{F3}");
+            }
+        })
     }
 }
 
+
 WLEvent.on("Scan", onScan);
-WLEvent.on("OnKey<000D>", onEnterKey);
+WLEvent.on("OnKey<000D>", toteCheck);
