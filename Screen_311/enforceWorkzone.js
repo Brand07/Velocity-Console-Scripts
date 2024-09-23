48ae0f94f93d4eb7b18ec14f8c4f11e15faa777d
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-23 10:05:46 -0500

    Change function to look for enter key

--- Screen_311/enforceWorkzone.js
+++ Screen_311/enforceWorkzone.js
@@ -8,35 +8,30 @@ Date: 9/23/2024
  */
 
 // Define the workzones based upon position on the screen
-var workZoneOne = Screen.getText(4,5,3);
-var workZoneTwo = Screen.getText(5,5,3);
-var workZoneThree = Screen.getText(6,5,3);
-var workZoneFour = Screen.getText(7,5,3);
-var workZoneFive = Screen.getText(8,5,3);
+//var workZoneOne = Screen.getText(4,5,4);
+//var workZoneTwo = Screen.getText(5,5,4);
+//var workZoneThree = Screen.getText(6,5,4);
+//var workZoneFour = Screen.getText(7,5,4);
+var workZoneFive = Screen.getText(8,5,4);
 
-View.toast("WorkZone One: " + workZoneOne);
-View.toast("WorkZone Two: " + workZoneTwo);
-View.toast("WorkZone Three: " + workZoneThree);
-View.toast("WorkZone Four: " + workZoneFour);
+
 View.toast("WorkZone Five: " + workZoneFive);
 
-// Function to check the WorkZone fields on scan
-function onScan(event) {
+// Function to check the WorkZone fields on Enter key press
+function onEnterKeyPress(event) {
     View.toast("Checking WorkZones");
-    //Check if one of the workzones has at least three characters inserted
-    if (workZoneOne.length > 3 || workZoneTwo.length > 3 || workZoneThree.length > 3 || workZoneFour.length > 3 || workZoneFive.length > 3) {
+    // Check if one of the workzones has at least three characters inserted
+    if (workZoneFive.length > 3) {
         setTimeout(function() {
             View.toast("Sending Enter Key");
             Device.sendKeys("{return}");
             View.toast("Enter Key Sent");
         }, 100);
-        setTimeout(function() {
-        }, 150);
-    }
-    else {
+    } else {
         View.toast("Please enter a WorkZone");
         event.data = "";
     }
 }
 
-WLEvent.on("Scan", onScan);
+// Attach the Enter key press event handler
+WLEvent.on("OnKey<000D>", onEnterKeyPress);
