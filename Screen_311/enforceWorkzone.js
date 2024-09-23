66fc27e7ba54a878289b6b1c6335b389c72b275f
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-23 08:54:09 -0500

    Add script purpose

---
+++ Screen_311/enforceWorkzone.js
@@ -0,0 +1,8 @@
+/*
+The purpose of this script is to enforce the user to enter
+a WorkZone instead of searching across all zones using
+a wildcard character.
+
+Author: Brandon Yates
+Date: 9/23/2024
+ */
