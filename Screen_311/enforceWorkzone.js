c14b784273afa63e2338f0a63521643873ac756d
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-23 12:16:57 -0500

    Dial in function logic

--- Screen_311/enforceWorkzone.js
+++ Screen_311/enforceWorkzone.js
@@ -11,17 +11,19 @@ Date: 9/23/2024
 //var workZoneOne = Screen.getText(4,5,4);
 //var workZoneTwo = Screen.getText(5,5,4);
 //var workZoneThree = Screen.getText(6,5,4);
-//var workZoneFour = Screen.getText(7,5,4);
+var workZoneFour = Screen.getText(7,6,4);
 var workZoneFive = Screen.getText(8,6,3);
 
 
 View.toast("WorkZone Five: " + workZoneFive);
 
+
+
 // Function to check the WorkZone fields on Enter key press
 function onScan(event) {
     View.toast("Checking WorkZones");
     // Check if one of the workzones has at least three characters inserted
-    if (workZoneFive.length < 3) {
+    if (workZoneFive.length == 0 || workZoneFour.length == 0) {
         setTimeout(function() {
             View.toast("Please enter a WorkZone");
             event.data = "";
