e6af527057a68a0db339dbf95a26060518d12d46
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-23 08:58:13 -0500

    Add workzone declarations

--- Screen_311/enforceWorkzone.js
+++ Screen_311/enforceWorkzone.js
@@ -6,3 +6,10 @@ a wildcard character.
 Author: Brandon Yates
 Date: 9/23/2024
  */
+
+// Define the workzones based upon position on the screen
+var workZoneOne = Screen.getText(4,5,3);
+var workZoneTwo = Screen.getText(5,5,3);
+var workZoneThree = Screen.getText(6,5,3);
+var workZoneFour = Screen.getText(7,5,3);
+var workZoneFive = Screen.getText(8,5,3);
