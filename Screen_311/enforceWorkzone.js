2c6119f9d89471dd1d36b6fc9dfaf89a681eb3b3
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-23 09:15:05 -0500

    Add toast messages to print the workzone status

--- Screen_311/enforceWorkzone.js
+++ Screen_311/enforceWorkzone.js
@@ -14,11 +14,17 @@ var workZoneThree = Screen.getText(6,5,3);
 var workZoneFour = Screen.getText(7,5,3);
 var workZoneFive = Screen.getText(8,5,3);
 
+View.toast("WorkZone One: " + workZoneOne);
+View.toast("WorkZone Two: " + workZoneTwo);
+View.toast("WorkZone Three: " + workZoneThree);
+View.toast("WorkZone Four: " + workZoneFour);
+View.toast("WorkZone Five: " + workZoneFive);
+
 // Function to check the WorkZone fields on scan
 function onScan(event) {
     View.toast("Checking WorkZones");
     //Check if one of the workzones has at least three characters inserted
-    if (workZoneOne.length >= 3 || workZoneTwo.length >= 3 || workZoneThree.length >= 3 || workZoneFour.length >= 3 || workZoneFive.length >= 3) {
+    if (workZoneOne.length > 3 || workZoneTwo.length > 3 || workZoneThree.length > 3 || workZoneFour.length > 3 || workZoneFive.length > 3) {
         setTimeout(function() {
             View.toast("Sending Enter Key");
             Device.sendKeys("{return}");
