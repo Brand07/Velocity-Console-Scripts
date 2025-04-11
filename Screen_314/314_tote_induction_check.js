da52cc83eadb59f15e384553206295d12ba694a4
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-04-11 08:35:30 -0500

    Refactor tote induction check logic for screens 314 and 315

--- Screen_314/314_tote_induction_check.js
+++ Screen_314/314_tote_induction_check.js
@@ -1,37 +1,77 @@
 /*
-This script is used to validate the tote ID scanned in the tote induction screen.
-The script checks if the tote ID starts with "TOT" and sends the Enter key if it does.
-If the scan starts with anything other than "TOT", the script displays an error message
-and prompts the user to rescan the tote ID.
+The purpose of this script is to solidify the differences
+between Screen 314 and Screen 315.
 
-Note: All Alphabetic and Numeric keys are disabled on this screen. The user can only scan the barcode to input.
-
+This script will check for the current screen it's on and determine what
+logical checks should be made to ensure the process stays on track and runs
+smoothly.
 
 Author: Brandon Yates
-Date: September 12th, 2024
+Date: 10/18/2024
  */
 
+/*
+Function to check what screen we're currently on.
+If 314, some logic will apply, if 315, other logic will apply.
+ */
 
 function onScan(event) {
-    // See if the input matches "TOT"
-    if (event.data.startsWith("TOT")) {
-        setTimeout(function() {
-            //View.toast("Sending Enter Key");
-            Device.sendKeys("{return}");
-            //View.toast("Enter Key Sent");
-        }, 100);
-        setTimeout(function() {
-            //View.toast("Sending Workzone");
-            //Device.sendKeys('*');
-            //View.toast("Setting Workzone");
-        }, 150);
+    // Retrieve the screen number
+    var screenNumber = Screen.getText(0, 0, 3);
+    var position = Screen.getCursorPosition();
+    View.toast("Screen Number: " + screenNumber);
+
+    // Check if the cursor is in the cont field on screen 314
+    if (screenNumber === "314" && position.row === 3) {
+        //0,0,3 = "314"
+        if (event.data.startsWith("0000")) {
+            setTimeout(function () {
+                Scanner.scanTerminator("NoAuto");
+                Device.sendKeys("{pause:300}{tab}" );
+                //View.toast("Sending Enter Key");
+                Device.sendKeys("{tab}");
+                //View.toast("Enter Key Sent");
+            }, 100);
+        }
+        else {
+            View.toast("Not a Container Number");
+            event.data = "";
+        }
     }
-    else {
-        View.toast("Not a Tote ID");
-        event.data = "";
+    // Check if we're on screen 314 in the tote id input field
+    else if (screenNumber === "314" && position.row === 6) {
+        //0,0,3 = "315"
+        if (event.data.startsWith("TOT")) {
+            setTimeout(function () {
+                //View.toast("Sending Enter Key");
+                Device.sendKeys("{return}");
+                //View.toast("Enter Key Sent");
+            }, 100);
+        }
+        else {
+            View.toast("Not a Tote ID");
+            event.data = "";
+        }
+    }
+    else if (screenNumber === "315" && position.row === 3) {
+        //0,0,3 = "315"
+        if (event.data.startsWith("TOT")) {
+            setTimeout(function () {
+                //View.toast("Sending Enter Key");
+                Device.sendKeys("{tab}");
+                Device.sendKeys("{return}");
+                //View.toast("Enter Key Sent");
+            }, 100);
+        }
+        else {
+            View.toast("Not a Tote ID");
+            event.data = "";
+        }
+    }
+    else if (screenNumber === "301") {
+        Device.sendKeys("{return}");
     }
 }
-
 
 // Disable A-Z (uppercase)
 const uppercaseLetters = [
@@ -72,8 +112,6 @@ if (Screen.getText(0, 0, 3) === "314") {
     disableKeys(lowercaseLetters);
     disableKeys(numbers);
 }
-
-
 
 // Attach the scan event handler
 WLEvent.on("Scan", onScan);
