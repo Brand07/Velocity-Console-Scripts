662e3a9a3aec4aa7c2381350a07b204135d4276c
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-05-06 13:56:20 -0500

    Init file

---
+++ Modern/Screen_314/314315Check.js
@@ -0,0 +1,18 @@
+function onScan(event) {
+    // Process the scan data
+    const data = event.data;
+    if (data.startsWith("0000")) {
+        View.toast("Container number scanned.");
+        setTimeout(function() {
+            // Disable auto-enter
+            Scanner.scanTerminator("NoAuto");
+            // Send tab key after a short delay
+            Device.sendKeys("{tab}");
+        }, 100);
+    } else {
+        event.data = "";
+        View.toast("Please scan a container number.");
+    }
+}
+
+WLEvent.on("Scan", onScan);
