78dade1ebc20971ff065ede2581847274ec09566
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-06-20 10:28:24 -0500

    Script functional in its intended area

--- Modern/Session/402.js
+++ Modern/Session/402.js
@@ -5,21 +5,16 @@ Purpose: To speed the 402 -> 401 Process
 */
 
 
-// Get the current screen number
-var screenNumber = Screen.getText(0,0,3);
-//0,0,3 = 402
-
-// Get the cursor's current position
-var position = Screen.getCursorPosition();
-var row = position.row;
 
 
 function onScan(event) {
     var text1 = Screen.getText(0,0,18); // "402 Relocate Query"
     var position = Screen.getCursorPosition();
+    var row = position.row;
+    //View.toast(event.data);
 
-    if (text1 === "402 Relocate Query" && position.row === 2) {
-        View.toast("Script working!")
+    if (text1 === "402 Relocate Query" && row === 2) {
+        View.toast("Script working!");
     }
 }
 
