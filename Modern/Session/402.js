5d4ca87d47c745205e3abdd04ca2ad45e2c7b41f
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-06-20 09:33:50 -0500

    Add basic logic for testing

--- Modern/Session/402.js
+++ Modern/Session/402.js
@@ -10,5 +10,18 @@ var screenNumber = Screen.getText(0,0,3);
 //0,0,3 = 402
 
 // Get the cursor's current position
+var position = Screen.getCursorPosition();
+var row = position.row;
 
+
+function onScan(event) {
+    var text1 = Screen.getText(0,0,18); // "402 Relocate Query"
+    var position = Screen.getCursorPosition();
+
+    if (text1 === "402 Relocate Query" && position === 2) {
+        View.toast("Script working!")
+    }
+}
+
+WLEvent.on("Scan", onScan);
 
