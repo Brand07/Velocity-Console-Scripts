a29f3e9992c8dbc71fb054753c833d465a2cac2b
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-06-20 09:22:18 -0500

    Init file

---
+++ Modern/Session/402.js
@@ -0,0 +1,14 @@
+/*
+Author: Brandon Yates
+Date: 6/20/2025
+Purpose: To speed the 402 -> 401 Process
+*/
+
+
+// Get the current screen number
+var screenNumber = Screen.getText(0,0,3);
+//0,0,3 = 402
+
+// Get the cursor's current position
+
+
