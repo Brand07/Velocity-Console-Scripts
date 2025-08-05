3833d93e805a9405d7c9d7fb13c29fa9debb2c83
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-08-05 09:53:24 -0500

    Format else statements to cover both cases

--- Modern/Screen_402/402_control_logic.js
+++ Modern/Screen_402/402_control_logic.js
@@ -4,7 +4,7 @@ Date: 8/5/2025
 Purpose: To speed the 402 -> 401 Process
 */
 
-View.toast("Currently on the 402 Screen");
+
 
 
 function onScan(event) {
@@ -13,20 +13,20 @@ function onScan(event) {
     var position = Screen.getCursorPosition();
     var row = position.row;
     View.toast(event.data);
-    
+
     setTimeout(function() {
         if (text1 === "402 Relocate Query" && row === 2) {
             View.toast("Script working!"); //Remove from Prod
             if (event.data.startsWith("T")) {
                 View.toast("Scan Validated"); // Remove from Prod
                 Device.sendKeys("{return}");
-        } else if (event.data.startsWith("0000") && row === 3) {
-            View.toast("Container scanned."); // Remove from Prod
-            Device.sendKeys("{return}");
-        }else{
-            event.data = "";
-            View.toast("Invalid Scan.");
-        }
+            } else if (event.data.startsWith("0000") && row === 3) {
+                View.toast("Container scanned."); // Remove from Prod
+                Device.sendKeys("{return}");
+            } else {
+                event.data = "";
+                View.toast("Invalid Scan.");
+            }
         }
     }, 1000);
 }
