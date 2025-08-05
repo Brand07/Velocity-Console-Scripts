1f08d96387a30d2e859ccc6a61ec1a299ceb92ee
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-08-05 09:43:33 -0500

    Add initial scan logic

--- Modern/Screen_402/402_control_logic.js
+++ Modern/Screen_402/402_control_logic.js
@@ -4,23 +4,29 @@ Date: 8/5/2025
 Purpose: To speed the 402 -> 401 Process
 */
 
-
+View.toast("Currently on the 402 Screen");
 
 
 function onScan(event) {
-    var text1 = Screen.getText(0,0,18); // "402 Relocate Query"
+    var text1 = Screen.getText(0,0,18); //
+    var text2 = Screen.getText(); // 
     var position = Screen.getCursorPosition();
     var row = position.row;
-    //View.toast(event.data);
-
+    View.toast(event.data);
+    
     setTimeout(function() {
-        if (text1 === "402 Relocate Query" && position.row === 2) {
+        if (text1 === "402 Relocate Query" && row === 2) {
             View.toast("Script working!"); //Remove from Prod
+            if (event.data.startsWith("T")) {
+                View.toast("Scan Validated"); // Remove from Prod
+                Device.sendKeys("{return}");
+        } else if (event.data.startsWith("0000") && row === 3) {
+            View.toast("Container scanned."); // Remove from Prod
             Device.sendKeys("{return}");
-            Device.sendKeys("{F2}");
-            Device.sendKeys("401");
-            Device.sendKeys("{return}");
-
+        }else{
+            event.data = "";
+            View.toast("Invalid Scan.");
+        }
         }
     }, 1000);
 }
