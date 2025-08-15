c8cd2fd26894d607cb2114955de4b0ae1aeea13e
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-08-15 08:10:41 -0500

    Refactor sortSession.js to enhance container validation and sound feedback

--- Modern/Session/Sort/sortSession.js
+++ Modern/Session/Sort/sortSession.js
@@ -3,3 +3,38 @@ Purpose: Controls all the logic for the Sort related screens.
 Date: 8/15/2025
 */
 
+function playSound(sound){
+    Device.beepPlayFile(sound);
+}
+
+function sendEnter(delay = 300) {
+    Device.sendKeys(`{pause:${delay}}{return}`);
+}
+
+function sendTab(delay = 300) {
+    Device.sendKeys(`{pause:${delay}}{TAB}`);
+}
+
+// Function to check the container number
+function checkContainer(scan_data) {
+    if (scan_data.length === 20 && scan_data.startsWith("0000")) {
+        return scan_data;
+    } else {
+        playSound("not_correct_container.mp3");
+        return;
+    }
+}
+
+
+function onScan(event){
+    var screenNumber = Screen.getText(0, 0, 4); // Get the screen number
+    var position = Screen.getCursorPosition(); // Get the cursor position
+    var row = position.row; // Get the current row
+
+    if(screenNumber === "704 " && row === 3){
+        var containerNumber = checkContainer(event.data);
+        if(containerNumber){
+            sendTab(300);
+        }
+    }
+}
