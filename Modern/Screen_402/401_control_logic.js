1a6146d5dc727513364cd59498ff514c3424e182
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-09-09 07:24:15 -0500

    Comment out debug toasts

--- Modern/Screen_402/401_control_logic.js
+++ Modern/Screen_402/401_control_logic.js
@@ -7,22 +7,22 @@ Purpose: To give the user an option to return to the 402 screen after using 401.
 View.toast("On the 401a screen.")
 
 function goToScreen(){
-    View.toast("About to send keys to go to 402.");
+    //View.toast("About to send keys to go to 402.");
     Device.sendKeys("{pause:2000}{F2}");
-    View.toast("Sent F2, now sending 402 and return.");
+    //View.toast("Sent F2, now sending 402 and return.");
     Device.sendKeys("{pause:300}{402}{return}");
-    View.toast("Keys sent.");
+    //View.toast("Keys sent.");
 }
 
 function openPrompt() {
     WLEvent.off("OnKey<000D>", openPrompt);
 
     Device.beepPlayFile("attention.mp3");
-    View.toast("Prompt about to show.");
+    //View.toast("Prompt about to show.");
 
     setTimeout(function() {
         Prompt.promptOptions("Alert", "Would you like to go back to 402?", "Yes|No", function(result) {
-            View.toast("Prompt result: " + result);
+            //View.toast("Prompt result: " + result);
             if (result === "Yes" || result === "ok") {
                 View.toast("Navigating to 402.");
                 goToScreen();
