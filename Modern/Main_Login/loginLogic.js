19ac6685ee2cee68adbbbe350ced97b765f6d1c3
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-10-06 08:25:37 -0500

    Refactor tab key sending into sendTab function

--- Modern/Main_Login/loginLogic.js
+++ Modern/Main_Login/loginLogic.js
@@ -3,12 +3,16 @@ Controls the login logic when scanning user credentials.
 This is applied at the main login screen.
 */
 
+function sendTab(delay = 300) {
+  Device.sendKeys(`{pause:${delay}}{TAB}`);
+}
+
 function onScan(event) {
   if (event.data === "") {
     Scanner.scanTerminator("NoAuto");
     View.toast("Please scan your credentials");
   } else {
-    Device.sendKeys({ tab });
+    sendTab();
   }
 }
 
