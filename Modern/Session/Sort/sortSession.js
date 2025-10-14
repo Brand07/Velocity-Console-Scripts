e9502ad6caef07e3640d8f7a59613b8bea0606f6
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-10-14 08:49:27 -0500

    Add logic for the 702a screen

--- Modern/Session/Sort/sortSession.js
+++ Modern/Session/Sort/sortSession.js
@@ -3,77 +3,83 @@ Purpose: Controls all the logic for the Sort related screens.
 Date: 8/15/2025
 */
 
-function playSound(sound){
-    Device.beepPlayFile(sound);
+function playSound(sound) {
+  Device.beepPlayFile(sound);
 }
 
 function sendEnter(delay = 300) {
-    Device.sendKeys(`{pause:${delay}}{return}`);
+  Device.sendKeys(`{pause:${delay}}{return}`);
 }
 
 function sendTab(delay = 300) {
-    Device.sendKeys(`{pause:${delay}}{TAB}`);
+  Device.sendKeys(`{pause:${delay}}{TAB}`);
 }
 
 // Function to check the container number
 function checkContainer(scan_data) {
-    if (scan_data.length === 20 && scan_data.startsWith("0000")) {
-        return scan_data;
-    } else {
-        playSound("not_correct_container.mp3");
-        return;
-    }
+  if (scan_data.length === 20 && scan_data.startsWith("0000")) {
+    return scan_data;
+  } else {
+    playSound("not_correct_container.mp3");
+    return;
+  }
 }
 
+function onScan(event) {
+  var screenNumber = Screen.getText(0, 0, 4); // Get the screen number
+  var position = Screen.getCursorPosition(); // Get the cursor position
+  var row = position.row; // Get the current row
 
-function onScan(event){
-    var screenNumber = Screen.getText(0, 0, 4); // Get the screen number
-    var position = Screen.getCursorPosition(); // Get the cursor position
-    var row = position.row; // Get the current row
-
-    //This correctly reflects old session code.
-    if(screenNumber === "704 " && row === 3){
-        var containerNumber = checkContainer(event.data);
-        if(containerNumber){
-            sendTab(300);
-        }
-    }else if(screenNumber === "704 " && row === 5){
-        if(event.data.startsWith("PID" || "PLT")){
-            //Tab down to the 'Type' field.
-            sendTab(300);
-            Device.sendKeys("PALS");
-            sendEnter(300);
-            return;
-        }else{
-            //Clear the scan data
-            event.data = "";
-            Scanner.scanTerminator("NoAuto");
-            View.toast("Invalid PID/PLT.");
-            playSound("invalid_pid.mp3");
-            return;
-        }
-        //Container Unpack Screen
-    }else if(screenNumber === "702 " && row === 2){
-        if(event.data === ""){
-            View.toast("Blank Scan");
-            Scanner.scanTerminator("NoAuto");
-
-        }else{
-            sendTab(300);
-        }
-    }else if(screenNumber === "702 " && row === 3){
-        var containerNumber = checkContainer(event.data);
-        if(containerNumber){
-            sendTab(300);
-            //Type "PALS" into the 'CnTp' field and hit enter
-            Device.sendKeys("PALS");
-            sendEnter(300);
-        }else{
-            View.toast("Invalid Container");
-            event.data = "";
-
-        }
+  //This correctly reflects old session code.
+  if (screenNumber === "704 " && row === 3) {
+    var containerNumber = checkContainer(event.data);
+    if (containerNumber) {
+      sendTab(300);
     }
+  } else if (screenNumber === "704 " && row === 5) {
+    if (event.data.startsWith("PID" || "PLT")) {
+      //Tab down to the 'Type' field.
+      sendTab(300);
+      Device.sendKeys("PALS");
+      sendEnter(300);
+      return;
+    } else {
+      //Clear the scan data
+      event.data = "";
+      Scanner.scanTerminator("NoAuto");
+      View.toast("Invalid PID/PLT.");
+      playSound("invalid_pid.mp3");
+      return;
+    }
+    //Container Unpack Screen
+  } else if (screenNumber === "702 " && row === 2) {
+    if (event.data === "") {
+      View.toast("Blank Scan");
+      Scanner.scanTerminator("NoAuto");
+    } else {
+      sendTab(300);
+    }
+  } else if (screenNumber === "702 " && row === 3) {
+    var containerNumber = checkContainer(event.data);
+    if (containerNumber) {
+      sendTab(300);
+      //Type "PALS" into the 'CnTp' field and hit enter
+      Device.sendKeys("PALS");
+      sendEnter(300);
+    } else {
+      View.toast("Invalid Container");
+      event.data = "";
+    }
+    //702a Unpack Container
+    // User can scan either the container, PID, or PLT.
+  } else if (screenNumber === "702a" && row === 5) {
+    if (event.data.startsWith("PID" || "PLT" || "0000")) {
+      sendEnter(300);
+    } else {
+      Scanner.scanTerminator("NoAuto");
+      View.toast("Invalid Scan.");
+    }
+  }
 }
 
 WLEvent.on("Scan", onScan);
