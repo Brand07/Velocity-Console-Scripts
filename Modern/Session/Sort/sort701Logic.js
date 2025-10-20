42503241274b4f912337a2385ea0b454d00afac4
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-10-20 08:49:33 -0500

    Add functions to send enter and tab keys. Add function to get device's IP

--- Modern/Session/Sort/sort701Logic.js
+++ Modern/Session/Sort/sort701Logic.js
@@ -4,3 +4,37 @@ Date: 10/20/2025
 Author: Brandon Yates
  */
 
+//Function to get the IP of the device
+function getDeviceIp(){
+    var ip = Network.getWifiAddress();
+    if (!ip || ip === "0.0.0.0"){
+        //Try alternative method
+        if (Network.getIPAddress){
+            ip = Network.getIPAddress();
+        }
+    }
+    return ip;
+}
+
+function sendEnter(delay = 250) {
+
+    if (typeof delay !== "number" || delay < 0) {
+        console.log("Invalid delay value");
+    }
+    Device.sendKeys(`{pause:${delay}}{return}`);
+}
+
+
+function sendTab(delay = 300){
+    if (typeof delay !== "number" || delay < 0){
+        console.log("Invalid delay value");
+    }
+    Device.sendKeys(`{pause:${delay}}{tab}`);
+}
+
+function showDebugMessage(message) {
+    View.toast(message);
+}
+
+d = showDebugMessage;
+
