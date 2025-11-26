04cc67bdcf38938eaffdc99ece6186671a213cad
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-11-26 11:53:19 -0600

    Add file

---
+++ Modern/Session/MH/mhSession.js
@@ -0,0 +1,15 @@
+/*
+Purpose: Controls all  the Material Handling logic
+Date: 11/26/2025
+ */
+
+//Function to send an enter key after a defined delay
+function sendEnter(delay=300){
+    Device.sendKeys(`{pause:${delay}}{return}`);
+}
+
+//Function to send a tab key after a defined delay
+function sendTab(delay=300){
+    Device.sendKeys(`{pause:${delay}}{tab}`);
+}
+
