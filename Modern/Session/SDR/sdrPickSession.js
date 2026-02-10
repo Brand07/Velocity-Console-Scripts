4df0415e11bcc1a43bfdf13b28c7b080c2077c53
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2026-02-10 09:55:17 -0600

    Remove valid_numbers list

--- Modern/Session/SDR/sdrPickSession.js
+++ Modern/Session/SDR/sdrPickSession.js
@@ -39,8 +39,6 @@ function disableScanner(){
 
 // ============ Variables ============
 
-let valid_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
 // ============ End Variables ============
 
 // ============ Main Function ============
