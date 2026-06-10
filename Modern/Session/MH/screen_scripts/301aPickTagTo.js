d725565937d50006be8f21ce55badc9beb98e7fb
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2026-06-10 08:59:42 -0500

    Add custom functions

--- Modern/Session/MH/screen_scripts/301aPickTagTo.js
+++ Modern/Session/MH/screen_scripts/301aPickTagTo.js
@@ -9,4 +9,19 @@ function showMessage(message) {
     View.toast(message);
 }
 
+function disableScanner() {
+    //Disable the scanner for 1 second
+    Scanner.enable(false);
+    setTimeout(function (){
+        Scanner.enable(true);
+    }, 1000);
+}
+
+function sendEnter(delay=300) {
+    Device.sendKeys(`{pause:${delay}}{return}`)
+}
+
+function sendTab(delay=300) {
+    Device.sendKeys(`{pause:${delay}}{return}`)
+}
 
