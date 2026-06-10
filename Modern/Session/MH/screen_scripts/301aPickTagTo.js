9a6530bbbef798f7a87a69cf16b36c1d06e5aa6f
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2026-06-10 08:55:25 -0500

    Init file

---
+++ Modern/Session/MH/screen_scripts/301aPickTagTo.js
@@ -0,0 +1,12 @@
+/*
+Purpose: To Prevent bad entry on the 301aPickTagTo screen
+Date: 6/10/2026
+*/
+
+//Custom Functions for remapping methods
+
+function showMessage(message) {
+    View.toast(message);
+}
+
+
